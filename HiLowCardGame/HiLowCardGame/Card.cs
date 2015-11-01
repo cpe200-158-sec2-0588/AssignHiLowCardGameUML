eb7434b13cbc09f92a2e5ce9b0879091c7be8864
pansitwattana <jjsqling@gmail.com>
2015-11-01 16:56:36 +0700

    570610588 Update Code, Update UML

---
+++ HiLowCardGame/HiLowCardGame/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiLowCardGame
+{
+    class Card
+    {
+        public static int DIAMOND = 1;
+        public static int CLUB = 2;
+        public static int HEART = 3;
+        public static int SPADE = 4;
+
+        public int Suit { get; set; }
+        public int Rank { get; set; }
+  
+
+        public Card(int Suit,int Rank)
+        {
+            this.Suit = Suit;
+            this.Rank = Rank;
+        }
+
+        public override string ToString()
+        {
+            if(Suit == DIAMOND)
+                return $"Diamond of {Rank}";
+            else if(Suit == CLUB)
+                return $"Club of {Rank}";
+            else if(Suit == HEART)
+                return $"Heart of {Rank}";
+            else
+                return $"Spade of {Rank}";
+
+        }
+        
+    }
+}
